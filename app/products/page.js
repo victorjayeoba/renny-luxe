444d32e6a09fdd1ca89836eeef1852a77e5752c6
Jayeoba Victor <victorjayeoba02@gmail.com>
2024-10-23 04:12:04 +0100

    fix

--- app/products/page.js
+++ app/products/page.js
@@ -3,16 +3,16 @@ import React, { Suspense } from 'react';
 import SearchComponent from '../component/SearchComponent';
 import { useSearchParams } from 'next/navigation';
 
-export async function generateMetadata({ searchParams }) {
+/* export async function generateMetadata({ searchParams }) {
   const { s } = searchParams; // Extract the search parameter
   return {
     title: `Search results for ${s}`, // Example of using the search term in the title
     description: `Find products related to ${s}`,
   };
-}
+} */
 
 function Page() {
-  const searchParams = useSearchParams(); // Get the search parameters
+  const searchParams = useSearchParams(); 
   const searchTerm = searchParams.get('s') || ""; 
 
   return (
