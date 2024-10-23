cbd823364a3c7871eedd06f7d56e6dd3699b949e
Jayeoba Victor <victorjayeoba02@gmail.com>
2024-10-23 04:07:17 +0100

    commit fix

--- app/products/page.js
+++ app/products/page.js
@@ -1,23 +1,24 @@
-"use client"
 // app/search/page.js (or the appropriate file path)
-import React from 'react';
+import React, { Suspense } from 'react';
 import SearchComponent from '../component/SearchComponent';
 import { useSearchParams } from 'next/navigation';
 
-/* export async function generateMetadata({ searchParams }) {
+export async function generateMetadata({ searchParams }) {
   const { s } = searchParams; // Extract the search parameter
   return {
     title: `Search results for ${s}`, // Example of using the search term in the title
     description: `Find products related to ${s}`,
   };
 }
- */
+
 function Page() {
   const searchParams = useSearchParams(); // Get the search parameters
   const searchTerm = searchParams.get('s') || ""; // Extract the 's' parameter or default to an empty string
 
   return (
-    <SearchComponent searchTerm={searchTerm} />
+    <Suspense fallback={<div>Loading...</div>}>
+      <SearchComponent searchTerm={searchTerm} /> {/* Pass the search term as a prop */}
+    </Suspense>
   );
 }
 
