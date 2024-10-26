5d1e2f82b21508202d32bba0bf8a12a88469d565
Jayeoba Victor <victorjayeoba02@gmail.com>
2024-10-26 13:52:53 +0100

    fix bugs

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <title>Rennys Luxe</title> 
-        <meta name="description" content="Welcome to [Your Brand Name], where beauty meets self-care! At [Your Brand Name], we believe that healthy, radiant skin is a reflection of inner wellness. Our curated collection of premium skincare products is designed to cater to all skin types and concerns, empowering you to embrace your natural beauty." />
+        <meta name="description" content="Welcome to Renny Luxe, where beauty meets self-care!, we believe that healthy, radiant skin is a reflection of inner wellness. Our curated collection of premium skincare products is designed to cater to all skin types and concerns, empowering you to embrace your natural beauty." />
         <meta name="keywords" content="skincare, beauty, self-care, healthy skin, moisturizers, serums, cleansers" />
         <meta name="author" content="Jayeoba VIctor" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" /> 
