482b4e23f90ad5f6c7d6715e517f98b09bac38b1
Jayeoba Victor <victorjayeoba02@gmail.com>
2024-11-22 01:44:33 +0100

    google seo

--- app/auth/profile/page.js
+++ app/auth/profile/page.js
@@ -1,37 +1,9 @@
 "use client"
-import React, { useState } from 'react'
-import { useSelector } from 'react-redux'
-import { useDispatch } from 'react-redux'
-import { login } from '@/app/features/user'
-import { logout } from '@/app/features/user'
-import { switchTheme } from '@/app/features/themeColor'
+import Profile from "@/app/component/Profle"
 function page() {
-  const [color , setColor] = useState("")
-  const user = useSelector((state)=>state.user.value)
-  const themeColor = useSelector((state)=>state.themeSwitch.value)
 
-  const dispatch = useDispatch()
   return (
-    <div className='text-lg font-normal' style={{color: themeColor}} >
-      <h2 className='text-2xl font-semibold' >Profle Page</h2>
-      <p>Name:{user.name}</p>
-      <p>Age:{user.age}</p>
-      <p>Email:{user.email}</p>
-      <button
-      onClick={()=>{
-        dispatch(login({ name: "Victor", age: 20, email: "victorjayeoba02@gmail.com" }))
-      }}
-      className="bg-red-400 p-2 px-5" >login</button>
-      <button onClick={()=> dispatch(logout())}  
-        className="bg-red-400 p-2 px-5"
-      >logout</button>
-
-      <input onChange={(e)=>setColor(e.target.value)} type='text' />
-      <button onClick={()=> dispatch(switchTheme(color))}  
-        className="bg-red-400 p-2 px-5"
-      >change color</button>
-    </div>
-
+   <Profile/>
   )
 }
 
