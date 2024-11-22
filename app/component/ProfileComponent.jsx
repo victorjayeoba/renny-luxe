8e720a647c3ead2d28dd21b6578cf405ff54663e
Jayeoba Victor <victorjayeoba02@gmail.com>
2024-11-22 01:53:02 +0100

    google seo

--- app/component/ProfileComponent.jsx
+++ app/component/ProfileComponent.jsx
@@ -5,7 +5,7 @@ import { useDispatch } from 'react-redux'
 import { login } from '@/app/features/user'
 import { logout } from '@/app/features/user'
 import { switchTheme } from '@/app/features/themeColor'
-export const ProfileComponent=()=> {
+export default const ProfileComponent=()=> {
   const [color , setColor] = useState("")
   const user = useSelector((state)=>state.user.value)
   const themeColor = useSelector((state)=>state.themeSwitch.value)
