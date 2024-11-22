995a14a559035feb0df0de09449e7f8c21fe4fbe
Jayeoba Victor <victorjayeoba02@gmail.com>
2024-11-22 02:18:26 +0100

    google seo

---
+++ next-sitemap.config.mjs
@@ -0,0 +1,6 @@
+module.exports = {
+    siteUrl: 'https://renny-luxe.vercel.app', // Replace with your domain
+    generateRobotsTxt: true, // (optional) Generates `robots.txt` file
+    sitemapSize: 5000, // Split sitemaps if you have a large number of pages
+  };
+  
